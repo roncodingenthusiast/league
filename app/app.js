70ab2d5df612ee9c0e6985dbaacb0e53eb023a34
Ronald Ekambi <rekambi@CEC000106.lan>
2017-11-26 16:16:25 -0500

    added teams to backend api and common

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var app = express();
 var db = require('./db');
 
 var UserController = require('./lib/user/UserController');
+var TeamController = require('./lib/team/TeamController');
 app.use('/users', UserController);
-
+app.use('/teams', TeamController);
 module.exports = app;
---
+++ app/lib/team/TeamController.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+var controller = require('../../db.js');
+
+router.post('/', function(req, res){
+	console.log(req.body);
+	var postData = {
+		name : req.body.name, 
+		email : req.body.email,
+		password : req.body.password
+	};
+	console.log('postData', postData);
+	controller.conn.getConnection(function(err, connection){
+		connection.query('insert into teams SET ?', postData, function(err, team){
+			if (err) {
+				console.log(err);
+				return res.status(500).send("There was a problem adding the information to the database.");
+			}
+			res.status(200).send(team);
+			connection.release();
+		});
+	});
+});
+
+router.get('/', function (req, res) {
+	controller.conn.getConnection(function(err, connection){
+		if(err){
+			console.log(err);
+			return res.status(501).send('could not obtain a connection');
+		}
+		connection.query('select * from teams', function (err, teams) {
+	        if (err) {
+	        	console.log(err);
+	        	connection.release();
+	        	return res.status(500).send("There was a problem finding the teams.");
+	        }
+	        console.log(teams);
+	        res.status(200).send(teams);
+	        connection.release();
+	    });
+
+	});
+    
+});
+
+router.get('/:id', function(req, res){
+	controller.conn.getConnection(function(err, connection){
+		console.log(req.params.id);
+		var postData = {
+			id : req.params.id
+		};
+		connection.query('select * from teams where id='+req.params.id, function (err, teams) {
+	        if (err) {
+	        	console.log(err);
+	        	connection.release();
+	        	return res.status(500).send("There was a problem finding the teams.");
+	        }
+	        res.status(200).send(teams);
+	        connection.release();
+	    });
+	});
+});
+
+router.delete('/:id', function(req, res){
+	controller.conn.getConnection(function(err, connection){
+		console.log(req.params.id);
+		var postData = {
+			id : req.params.id
+		};
+		connection.query('delete from teams where id='+req.params.id, function (err, teams) {
+	        if (err) {
+	        	console.log(err);
+	        	connection.release();
+	        	return res.status(500).send("There was a problem finding the teams.");
+	        }
+	        res.status(200).send(teams);
+	        connection.release();
+	    });
+	});
+});
+
+router.put('/:id', function(req, res){
+	controller.conn.getConnection(function(err, connection){
+		console.log(req.params.id);
+		var postData = {
+			name: 'update'
+		};
+		connection.query('update teams set ? where id='+req.params.id, postData, function (err, teams) {
+	        if (err) {
+	        	console.log(err);
+	        	connection.release();
+	        	return res.status(500).send("There was a problem finding the teams.");
+	        }
+	        res.status(200).send(teams);
+	        connection.release();
+	    });
+	});
+});
+module.exports = router;
