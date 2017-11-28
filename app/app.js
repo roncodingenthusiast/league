946d90893f3a2f6385ff3d26a38ca3f06068f3e6
Ronald Ekambi <ronekambi@gmail.com>
2017-11-28 09:57:21 -0500

    Added font-awesome

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ var app = express();
 var db = require('./db');
 
 app.use('/public', express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modules/font-awesome/css'));
+app.use('/fonts', express.static(__dirname + '/node_modules/font-awesome/fonts'));
 
 var UserController = require('./lib/user/UserController');
 var TeamController = require('./lib/team/TeamController');
