a9272d8f5fec13cc5c862320d11cde054f24266d
Ronald Ekambi <ronekambi@gmail.com>
2017-11-28 06:14:27 -0500

    [Added] bootstrap deps

--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+app.use('/public', express.static(__dirname + '/public'));
+
 var UserController = require('./lib/user/UserController');
 var TeamController = require('./lib/team/TeamController');
 app.use('/users', UserController);
 app.use('/teams', TeamController);
-//console.log('dirname: '+ __dirname + '/public');
-app.use('/public', express.static(__dirname + '/public'));
 module.exports = app;
