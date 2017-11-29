9a41ce6ab7696e84317954df56bd69d368c599c7
Ronald Ekambi <ronekambi@gmail.com>
2017-11-29 18:07:54 -0500

    removed circle ci

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 process.env.NODE_ENV = 'test';
-let server = require('../server');
+var assert = require('assert');
 describe("Hello World Server", function() {
 	
 	describe("GET /", function() {
-		process.exit(0);
+		it('should return -1 when the value is not present', function() {
+			assert.equal(-1, [1,2,3].indexOf(4));
+		  });
 	});	
 
 });
