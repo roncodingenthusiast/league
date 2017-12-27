b3c6cdb12540a3d1eadf172b4bffb527f3ac4897
Ronald Ekambi <ronekambi@gmail.com>
2017-12-27 06:42:00 -0500

    backend validation working

--- public/controllers/teamscontroller.js
+++ public/controllers/teamscontroller.js
@@ -1,8 +1,19 @@
 app.controller('teamsCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.teamToSave = {};
-
+	var config = {
+		headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+	};
     $scope.saveTeam = function(submittedTeam) {
 		$scope.teamToSave = angular.copy(submittedTeam);
+		$http.post('/teams', $.param($scope.teamToSave), config)
+		.then(
+			function sucess(data){
+		
+			},
+			function failed(err){
+		
+			}
+		);
     };
 	$scope.resetForm = function() {
 		$scope.team = angular.copy({});
