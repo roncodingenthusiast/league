4b0fea270bf0bdef912e7152347c2e34db32caed
Ronald Ekambi <ronekambi@gmail.com>
2017-12-27 05:13:20 -0500

    Create team form front-end

--- public/controllers/teamscontroller.js
+++ public/controllers/teamscontroller.js
@@ -1,3 +1,11 @@
 app.controller('teamsCtrl', ['$scope', '$http', function($scope, $http){
-	
+	$scope.teamToSave = {};
+
+    $scope.saveTeam = function(submittedTeam) {
+		$scope.teamToSave = angular.copy(submittedTeam);
+    };
+	$scope.resetForm = function() {
+		$scope.team = angular.copy({});
+	};
+    $scope.resetForm();
 }]);
