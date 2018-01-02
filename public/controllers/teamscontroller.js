a71ae7c0f0605ef66454e49e63811ffb7a36ed1e
Ronald Ekambi <ronekambi@gmail.com>
2018-01-02 00:41:37 -0500

    added these things

--- public/controllers/teamscontroller.js
+++ public/controllers/teamscontroller.js
@@ -1,10 +1,32 @@
 app.controller('teamsCtrl', ['$scope', '$http', '$location', 'HeadersConfig', function($scope, $http, $location, HeadersConfig){
 	$scope.teams = [];
+
+	function getRandomArbitrary(min, max) {
+		return Math.random() * (max - min) + min;
+	}
 	$scope.queryAllTeams = function() {
 		$http.get('/teams', HeadersConfig.getConfig())
 		.then(
 			function success(response){
 				$scope.teams = response.data;
+				var teamsId = [];
+				if($scope.teams.length % 2 !== 0){
+					$scope.teams.push({
+						id: 'bye',
+						teamname: 'bye',
+						color: 'bye',
+						captainemail: 'bye@email.com',
+						league: 'bye',
+						level: 'bye'
+					});
+					//https://en.wikipedia.org/wiki/Round-robin_tournament
+				}
+				$scope.teams.forEach(function(element) {
+					teamsId.push(element.id);
+					element.opponents = [];
+				});
+				console.log(teamsId);
+				console.log($scope.teams);
 			},
 			function failed(err){
 
