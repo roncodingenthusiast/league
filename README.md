4e357a5931ee4f8b940df2d6b3069e496a4e445e
Ronald Ekambi <ronekambi@gmail.com>
2018-02-24 12:31:29 -0500

    added read me

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # league manager pro
+
+Basic features: 
+
+- Create tournaments 
+- Create teams for tournaments 
+- Randomize games for tournament 
+- Create points table for tournaments
+
+Secondary Features: 
+
+- Create players for teams 
+- Create locations for tournaments
+- Create payment module 
+
+
--- public/config/config.js
+++ public/config/config.js
@@ -5,35 +5,56 @@ app.config(function($routeProvider){
 		templateUrl: 'public/templates/home.html',
 		controller: 'mainCtrl'
 	})
-	//teams
-	.when('/teams/new',{
-		templateUrl: 'public/templates/teams/new.html', 
-		controller: 'teamsCtrl'
+	//league list
+	.when('/league',{
+		templateUrl: 'public/templates/leagues/new.html', 
+		controller: 'leagueCtrl'
 	})
-	.when('/teams/list',{
-		templateUrl: 'public/templates/teams/list.html', 
-		controller: 'teamsCtrl'
-	})
-	//schedule/games
-	.when('/schedule', {
-		templateUrl: 'public/templates/games/list.html',
-		controller: 'gamesCtrl'
-	})
-	//league 
+	//create a new league
 	.when('/league/new',{
 		templateUrl: 'public/templates/leagues/new.html', 
 		controller: 'leagueCtrl'
 	})
+	//edit a league
+	.when('/league/:id/edit',{
+		templateUrl: 'public/templates/leagues/new.html', 
+		controller: 'leagueCtrl'
+	})
+	//add a team to a league
+	.when('/league/:id/teams/new',{
+		templateUrl: 'public/templates/leagues/new.html', 
+		controller: 'leagueCtrl'
+	})
+	//list of teams in league
+	.when('/league/:id/teams',{
+		templateUrl: 'public/templates/leagues/new.html', 
+		controller: 'leagueCtrl'
+	})
+	// //teams
+	// .when('/teams/new',{
+	// 	templateUrl: 'public/templates/teams/new.html', 
+	// 	controller: 'teamsCtrl'
+	// })
+	// .when('/teams/list',{
+	// 	templateUrl: 'public/templates/teams/list.html', 
+	// 	controller: 'teamsCtrl'
+	// })
+	// //schedule/games
+	// .when('/schedule', {
+	// 	templateUrl: 'public/templates/games/list.html',
+	// 	controller: 'gamesCtrl'
+	// })
+	
 
-	//players
-	.when('/team/:id/players/list', {
-		templateUrl: 'public/templates/players/list.html', 
-		controller: 'playersCtrl'
-	})
-	.when('/team/:id/players/new', {
-		templateUrl: 'public/templates/players/new.html', 
-		controller: 'playersCtrl'
-	})
+	// //players
+	// .when('/team/:id/players/list', {
+	// 	templateUrl: 'public/templates/players/list.html', 
+	// 	controller: 'playersCtrl'
+	// })
+	// .when('/team/:id/players/new', {
+	// 	templateUrl: 'public/templates/players/new.html', 
+	// 	controller: 'playersCtrl'
+	// })
 	//if the route is not recognised then it redirects you to home page
 	.otherwise({
 		redirecTo: '/'
