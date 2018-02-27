2301b52a2738f5562038213258d6a367025c3c17
Ronald Ekambi <ronekambi@gmail.com>
2018-02-27 07:55:03 -0500

    finished get and post for league

--- public/controllers/leaguecontroller.js
+++ public/controllers/leaguecontroller.js
@@ -1,6 +1,5 @@
 app.controller('leagueCtrl', ['$scope', '$http', '$location', '$routeParams', 'HeadersConfig',
 	function($scope, $http, $location, $routeParams, HeadersConfig){
-
 	$scope.queryAllLeagues = function(){
 		$scope.leagues = [];
 		$http
@@ -12,7 +11,23 @@ app.controller('leagueCtrl', ['$scope', '$http', '$location', '$routeParams', 'H
 			console.log('here is the reason for failure', error);
 		});
 	};
+	
+	$scope.leaveToSave = {};
+	$scope.saveLeague = function(leaguePostData){
+		console.log('postData', leaguePostData);
+		$scope.leaveToSave = angular.copy(leaguePostData);
+		$http.post('/leagues', $.param($scope.leaveToSave), HeadersConfig.getConfig())
+		.then(
+			function success(data){
+				$location.path('/league');
+			},
+			function failed(err){
+		
+			}
+		);
+	};
+	console.log($scope.leagues);
+	//$scope.leagueToSave = HeadersConfig.resetForm();
 	$scope.queryAllLeagues();
-	$scope.leagueToSave = {};
-	$scope.leagueToSave = HeadersConfig.resetForm();
+	console.log($scope.leagues);
 }]);
