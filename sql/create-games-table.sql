cc3b7ab7dda5c54ed66dbd83e47c9b48d70e3d64
Ronald Ekambi <ronekambi@gmail.com>
2018-03-23 16:58:05 -0400

    added stuff for table

--- sql/create-games-table.sql
+++ sql/create-games-table.sql
@@ -1,11 +1,33 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:7777
+-- Generation Time: Mar 23, 2018 at 09:57 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `pro_league_manager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `league`
+--
+
 CREATE TABLE `league` (
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
-  `number_of_teams` int(11),
+  `number_of_teams` int(11) DEFAULT NULL,
   `difficulty_level` text,
   `week_day` text,
   `location` text,
-  `id` integer primary key AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `league_name` text,
   `league_manager` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -14,6 +36,28 @@ CREATE TABLE `league` (
 -- Dumping data for table `league`
 --
 
-INSERT INTO `league` (`start_date`, `end_date`, `number_of_teams`, `difficulty_level`, `week_day`, `location`, `league_name`, `league_manager`) VALUES
-('2018-02-01', '2018-02-28', 10, 'Medium Rec', 'Thursday', 'Lees Ottawa U ', 'Medium Rec - LEague name', 'thom'),
-('2018-02-01', '2018-02-28', 10, 'Medium Rec', 'Thursday', 'Lees Ottawa U ', 'Medium Rec - LEague name', 'thom');
+INSERT INTO `league` (`start_date`, `end_date`, `number_of_teams`, `difficulty_level`, `week_day`, `location`, `id`, `league_name`, `league_manager`) VALUES
+('2018-02-01', '2018-02-28', 10, 'Medium Rec', 'Thursday', 'Lees Ottawa U ', 1, 'Medium Rec - LEague name', 'thom'),
+('2018-02-01', '2018-02-28', 10, 'Medium Rec', 'Thursday', 'Lees Ottawa U ', 2, 'Medium Rec - LEague name', 'thom'),
+('0000-00-00', '0000-00-00', 23, 'Recreational', 'tuesday', 'sdf', 3, 'league', NULL),
+('0000-00-00', '0000-00-00', 12, 'Beginner / Recreational', 'monday', 'sdf', 4, 'league', 'manager');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `league`
+--
+ALTER TABLE `league`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `league`
+--
+ALTER TABLE `league`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
