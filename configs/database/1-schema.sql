7dd4f966ff45fcc1b2ec1d7e0d8c5f5adc2f269d
Ronald Ekambi <ronekambi@gmail.com>
2018-04-07 01:38:34 -0400

    made leagues and games table

--- configs/database/1-schema.sql
+++ configs/database/1-schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS pro_league_manager;
 CREATE USER 'league'@'%';
 GRANT ALL PRIVILEGES ON *.* To 'league'@'%' IDENTIFIED BY 'league';
 
-CREATE TABLE `league` (
+CREATE TABLE `leagues` (
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
   `number_of_teams` int(11) DEFAULT NULL,
@@ -26,4 +26,15 @@ CREATE TABLE `teams` (
   `captain_email` text,
   `id` int(11) NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `games` (
+  `game_date` date NOT NULL,
+  `home_team_id` int(11),
+  `away_team_id` int(11),
+  `league_id` int(11),
+  `field` text,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `referree_id` int(11),
+     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
